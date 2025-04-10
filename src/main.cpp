f5c628d7b428d49f64397be8657c32bd54539a24
Thomas Helland <thomas.helland97@gmail.com>
2025-04-10 13:33:22 +0200

    Can now generate uint data

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
-#include "include/BinarySearch.hpp"
-
+// #include "include/BinarySearch.hpp"
+#include "include/UniqueIntGen.hpp"
 
 int main(int argc, char* argv[])
 {
-  BinarySearch b;
+  // BinarySearch b;
 
-  b.binary_search(b.items, 13);
+  // b.binary_search(b.items, 13);
 
+  UniqueIntGen i;
+
+  i.uint_10k_gen(false);
+  
   return 0;
 }    
