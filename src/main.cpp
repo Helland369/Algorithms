43c2a0d5f009384a0a6b1ed5a2afba7ea68ba85d
Thomas Helland <thomas_helland@pm.me>
2025-04-23 14:34:18 +0200

    Added timing for the algorithms

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "include/BubbleSort.hpp"
 #include "include/MergeSort.hpp"
 #include "include/JumpSearch.hpp"
+#include <chrono>
 #include <cstdint>
 #include <cstdlib>
 #include <iostream>
@@ -35,44 +36,71 @@ int main(int argc, char* argv[])
     std::cout << "*** Algorithms ***\n" << "[1] Bubble sort\n" << "[2] Merge sort\n" << "[3] Binary search\n" << "[4] Jump search\n" << "[0] EXIT\n" << "\n";
 
     std::cin >> input;
+
+    // timing helpe
+    auto mesure_time = [](auto&& func) {
+      auto start = std::chrono::high_resolution_clock::now();
+      func();
+      auto stop = std::chrono::high_resolution_clock::now();
+      return std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+    };
     
     switch (input)
     {
     case 0:
-      std::exit(0);
-      break;
+      return 0;
     case 1:
-      buble.bubble_sort(vec, 10000);
-      for (auto y : vec)
       {
-        std::cout << y << "\n";
-      }    
-      break;
+        auto duration = mesure_time([&]() {
+          buble.bubble_sort(vec, 10000);
+        });
+        for (auto y : vec)
+        {
+          std::cout << y << "\n";
+        }
+        std::cout << "Microseconds: " << duration << "\n";
+        break;
+      }
     case 2:
-      m.merge_sort(vec, 0, vec.size() - 1);
-      for (auto y : vec)
       {
-        std::cout << y << "\n";
-      }    
-      break;
+        auto duration = mesure_time([&]() {
+          m.merge_sort(vec, 0, vec.size() - 1);
+        });
+        for (auto y : vec)
+        {
+          std::cout << y << "\n";
+        }
+        std::cout << "Microseconds: " << duration << "\n";
+        break;
+      }
     case 3:
-      key = input_option();
-      bin.binary_search(sorted_vec, key);
-      key = 0;
-      break;
+      {
+        key = input_option();
+        auto duration = mesure_time([&]() {
+          bin.binary_search(sorted_vec, key);
+        });
+        std::cout << "Microseconds: " << duration << "\n";
+        key = 0;
+        break;
+      }
     case 4:
-      key = input_option();
-      index = jump.jump_search(sorted_vec, sorted_vec.size(), key);
-      if (index != -1)
       {
-        std::cout << key << " was found at index: " << index << "\n";
+        key = input_option();
+        auto duration = mesure_time([&]() {
+          index = jump.jump_search(sorted_vec, sorted_vec.size(), key);
+        });
+        if (index != -1)
+        {
+          std::cout << key << " was found at index: " << index << "\n";
+        }
+        else
+        {
+          std::cout << key << " was not found..,\n";
+        }
+        std::cout << "Millisedconds: " << duration << "\n";
+        key = 0;
+        break;
       }
-      else
-      {
-        std::cout << key << " was not found..,\n";
-      }
-      key = 0;
-      break;
     default:
       std::cout << "Invalid input!" << "\n";
       break;
